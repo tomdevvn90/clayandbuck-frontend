b0307e773b9255e82fb8562e1e242da01ac48e63
Trần Công Tập <trancongtap@Trans-Mac-mini.local>
2023-06-30 10:14:57 +0700

    Test git push

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
@@ -15,7 +14,8 @@ export default function Index({ allPosts: { edges }, preview }) {
   return (
     <Layout preview={preview}>
       <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+        <title>The Clay Travis  & Buck Sexton Show</title>
+        <meta name="description" content="Clay Travis and Buck Sexton tackle the biggest stories in news, politics and current events with intelligence and humor."></meta>
       </Head>
       <Container>
         <Intro />
