7183b1a6823b61a05a41b5ef855614eb5dfeafca
Tom Pham <tomdevvn90@gmail.com>
2023-07-24 23:04:02 +0700

    fix issue on Optout page

--- pages/optout.tsx
+++ pages/optout.tsx
@@ -1,57 +1,66 @@
-import React from 'react'
-import Head from 'next/head'
-import ErrorPage from 'next/error'
-import Container from '../components/container'
-import Layout from '../components/layout/layout'
-import { getPageData } from '../lib/graphql-api'
-import { useRouter } from 'next/router'
-import { ParseHtmlToReact } from '../utils/parse-html-to-react'
-import { SITE_URL } from '../lib/constants'
-import Optout from '../components/optout/published-notice-sdk'
-
-export default function Page( {pageData} ) {
-
-	const page = pageData?.pageBy ?? {}
-	const router = useRouter()
-	if (!router.isFallback && !page?.slug) {
-		return <ErrorPage statusCode={404} />
-	}
-
-	const { headerMenu, footerMenu } = pageData
-	const { templateName } = page?.template ?? ''
-	const pageClass = templateName? templateName.toLowerCase().replace(' ', '-') : ''
-
-	const { seo } = page
-  	const fullHead = ParseHtmlToReact(seo.fullHead);
-	const cleanPath = router.asPath.split('#')[0].split('?')[0];
-	const canonicalUrl = `${SITE_URL}` + (router.asPath === '/' ? '' : cleanPath);
-
-	return (
-	  <Layout headerMenu={headerMenu} footerMenu={footerMenu}>
-		<Head>
-		  {fullHead}
-		  <link rel="canonical" href={canonicalUrl} />
-		  <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"></meta>
-		  <meta name="twitter:image" content={page.seoTwitterThumb} />
-		  <meta name="twitter:image:width" content="1200" />
-		  <meta name="twitter:image:height" content="640" />
-		</Head>
-		<div className={`main-wrap page ${pageClass}`}>
-            <Container>
-                <Optout />
-            </Container>
-		</div>
-		
-	  </Layout>
-	)
+import React from "react";
+import Head from "next/head";
+import ErrorPage from "next/error";
+import Container from "../components/container";
+import Layout from "../components/layout/layout";
+import { getPageData } from "../lib/graphql-api";
+import { useRouter } from "next/router";
+import { ParseHtmlToReact } from "../utils/parse-html-to-react";
+import { SITE_URL } from "../lib/constants";
+import dynamic from "next/dynamic";
+// import Optout from '../components/optout/published-notice-sdk'
+const Optout = dynamic(
+  () => import("../components/optout/published-notice-sdk"),
+  {
+    ssr: false,
   }
-  
-  /** Server-side Rendering (SSR) */
-  export async function getServerSideProps() {
-	 const pageData = await getPageData( '/optout' );
+);
 
-	 return {
-		props: {pageData}
-	 }
+export default function Page({ pageData }) {
+  const page = pageData?.pageBy ?? {};
+  const router = useRouter();
+  if (!router.isFallback && !page?.slug) {
+    return <ErrorPage statusCode={404} />;
   }
-  
+
+  const { headerMenu, footerMenu } = pageData;
+  const { templateName } = page?.template ?? "";
+  const pageClass = templateName
+    ? templateName.toLowerCase().replace(" ", "-")
+    : "";
+
+  const { seo } = page;
+  const fullHead = ParseHtmlToReact(seo.fullHead);
+  const cleanPath = router.asPath.split("#")[0].split("?")[0];
+  const canonicalUrl = `${SITE_URL}` + (router.asPath === "/" ? "" : cleanPath);
+
+  return (
+    <Layout headerMenu={headerMenu} footerMenu={footerMenu}>
+      <Head>
+        {fullHead}
+        <link rel="canonical" href={canonicalUrl} />
+        <meta
+          name="robots"
+          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+        ></meta>
+        <meta name="twitter:image" content={page.seoTwitterThumb} />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="640" />
+      </Head>
+      <div className={`main-wrap page ${pageClass}`}>
+        <Container>
+          <Optout />
+        </Container>
+      </div>
+    </Layout>
+  );
+}
+
+/** Server-side Rendering (SSR) */
+export async function getServerSideProps() {
+  const pageData = await getPageData("/optout");
+
+  return {
+    props: { pageData },
+  };
+}
